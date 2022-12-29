4a1151f5f2b8920edec9e0cb148b5d73c14a9677
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2022-12-29 14:56:38 +0100

    removed out folder from git and edited gitignore

--- out/_next/static/e__DeZTNKf_d-05y-URFn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b4b87dc3ce5ea0bbd258.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-73a996dab678f31e3f83.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
